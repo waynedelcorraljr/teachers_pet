c4b5cddd5f1a248744caf79c41144ab678cfc22d
Wayne del Corral Jr <wdelcorjr@gmail.com>
2021-01-21 00:47:14 -0600

    gets rid of img files, adds visual validations to Course, creates _form for course, changes img for sign up w/ google

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -4,18 +4,20 @@ class CoursesController < ApplicationController
         if session[:user_id]
             @course = Course.new(user_id: session[:user_id]) 
         else
+            flash[:alert] = "Must be logged in to create courses."
             redirect_to signin_path
         end
     end
     
     def create 
         @course = Course.new(course_params)
-        raise @course.errors.inspect
-
+        
         if @course.save
             redirect_to user_path(@course.user)
         else
+            flash[:alert] = "Must complete all fields."
             render :new
+            # raise @course.errors.inspect
         end
     end
     
